9a1ddc6ccaa94d04910ac6d7046ba7d8ef53ab62
julmarti <julmarti@42.student.fr>
2021-03-15 08:56:33 +0100

    ébauche

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,34 @@
 /*   By: julmarti <julmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 16:13:44 by julmarti          #+#    #+#             */
-/*   Updated: 2021/03/11 15:45:46 by julmarti         ###   ########.fr       */
+/*   Updated: 2021/03/12 16:24:40 by julmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <bsd/string.h>
+#define BUFFER_SIZE 100
 
 int		get_next_line(int fd, char **line)
 {
-	size_t	retread;
 	int		i;
 	int		j;
-	char	*fdline;
+	size_t	retread;
+	char	*str_save;
 
 	i = 0;
-	j = 0;
-//	line = (char **)malloc(sizeof(char *) * BUFFER_SIZE);
-//	fdline = (char *)malloc(sizeof(char) * ft_strlen(fdline));
 	retread = read(fd, *line, BUFFER_SIZE);
-	if (!fdline || !line || fd > 0 || retread < 0 ) //si les malloc échouent ou si fd existe ou si read échoue,
+	if (!line || fd > 0 || retread < 0 ) //si les malloc échouent ou si fd existe ou si read échoue,
 		return (-1); // alors return -1 pour signifier un.e erreur 
 	if (retread == 0) // si on a atteint la fin du fichier
 		return(0);
 	else
 	{
-		while (retread > 0 && fdline[j] != '\0') // tant que read ne retourne pas 0 ça signifie que l'on a pas atteint la fin du fichier
+		str_save = strchr(*line, 10); //je place mon curseur sur l'emplacement de la première occurence \n dans line 
+		while (retread > 0) // tant que read ne retourne pas 0 ça signifie que l'on a pas atteint la fin du fichier
 		{
-			line[i][j] = fdline[j];// on stocke la ligne lue dans le tableau line
+			
+			str_save[j] = line[i][j]; // on stocke la ligne lue dans le tableau line
 			j++;
 		}
 		return (1);
